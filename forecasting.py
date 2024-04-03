06f0a2bdbfcfa15b1b39ae1c9c658c90f65a3d74
r.repin <r.repin@keyauto.ru>
2024-04-03 16:08:41 +0300

    [feature] add tasks

--- forecasting.py
+++ forecasting.py
@@ -1,28 +1,43 @@
-# import logging
-# import threading
-# import subprocess
-# import multiprocessing
+from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import Manager
+from multiprocessing import Pool
+from os import cpu_count
 
-
-from external.client import YandexWeatherAPI
+# from external.client import YandexWeatherAPI
 from tasks import (
     DataFetchingTask,
     DataCalculationTask,
     DataAggregationTask,
     DataAnalyzingTask,
 )
-from utils import CITIES, get_url_by_city_name
+from utils import CITIES
 
 
-def forecast_weather():
+def forecast_weather() -> None:
     """
     Анализ погодных условий по городам
     """
-    # city_name = "MOSCOW"
-    # url_with_data = get_url_by_city_name(city_name)
-    # resp = YandexWeatherAPI.get_forecasting(url_with_data)
-    # print(resp)
-    pass
+    manager = Manager()
+    data_aggregation_queue = manager.Queue()
+
+    # Получение данных через API
+    data_fetching_task = DataFetchingTask()
+    with ThreadPoolExecutor() as executor:
+        fetched_data = executor.map(data_fetching_task, list(CITIES.keys()))
+
+    # Вычисление погодных параметров
+    data_calculation_task = DataCalculationTask(queue=data_aggregation_queue)
+    with Pool(processes=cpu_count()) as pool:
+        pool.map(data_calculation_task, fetched_data)
+
+    # Объединение вычисленных данных
+    data_aggregation_task = DataAggregationTask(queue=data_aggregation_queue)
+    data_aggregation_task.start()
+    data_aggregation_task.join()
+
+    # Финальный анализ и получение результата
+    data_analyzing_task = DataAnalyzingTask()
+    data_analyzing_task()
 
 
 if __name__ == "__main__":
